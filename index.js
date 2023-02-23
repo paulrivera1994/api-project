c99d522fa6fc28a3498eeb0af02df704151856ab
Paul <juniorrivera81@yahoo.com>
2023-02-23 16:04:49 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const port = 5035;
+
+const cars = [
+  { brand: "Dodge", car: "Charger" },
+  { brand: "Ford", car: "Mustang" },
+];
+
+app.get("/", (req, res) => {
+  res.json(cars);
+});
+
+app.get("/cars/:id", (req, res) => {
+  res.json(cars[parseInt(req.params.id) - 1]);
+});
+
+app.post("/cars", (req, res) => {
+  console.log(req.body);
+  res.json({ message: "ok" });
+});
+
+app.put("/dogs/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  res.json({ message: `updated car ${req.params.id}` });
+});
+app.delete("/dogs/:id", (req, res) => {
+  console.log(req.params.id);
+  res.json({ message: `deleting car ${req.params.id}` });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
